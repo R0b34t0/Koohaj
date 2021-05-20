d15787d2990fcd3c099a8ba04e40492b53c83c58
Alen Abdulahović <alen.abdulahovic1@gmail.com>
2021-05-20 14:37:11 +0200

    Sitna promjena za API

--- firebase.js
+++ firebase.js
@@ -9,20 +9,7 @@ import 'firebase/firestore'
 // firebase init - add your own config here
 const firebaseConfig = {
   // API PODACI
-<<<<<<< HEAD
-  apiKey: "AIzaSyD8JAsVVAuXGGA3CAkgEFY7BnggYsqjMBg",
-=======
-    apiKey: "AIzaSyD8JAsVVAuXGGA3CAkgEFY7BnggYsqjMBg",
->>>>>>> development
-    authDomain: "mealfind-testdb.firebaseapp.com",
-    projectId: "mealfind-testdb",
-    storageBucket: "mealfind-testdb.appspot.com",
-    messagingSenderId: "710487185799",
-    appId: "1:710487185799:web:32942a1e9435370be31b22"
-<<<<<<< HEAD
-=======
-
->>>>>>> development
+ 
 }
 firebase.initializeApp(firebaseConfig)
 
